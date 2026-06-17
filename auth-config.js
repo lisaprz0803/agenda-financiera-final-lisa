d6ebe506c57b9465335db762a32c2be6208aeaf2
lisaprz0803 <294371049+lisaprz0803@users.noreply.github.com>
2026-06-17 00:18:37 -0400

    Configure Google OAuth access

--- auth-config.js
+++ auth-config.js
@@ -1,4 +1,4 @@
 window.AGENDA_AUTH_CONFIG = {
-  googleClientId: "PEGA_AQUI_TU_GOOGLE_CLIENT_ID",
-  allowedEmails: ["tu-correo@gmail.com"]
+  googleClientId: "157406915690-1b0mlv03tppenmtmbout9e9rmedlgtbb.apps.googleusercontent.com",
+  allowedEmails: ["lisaprz0803@gmail.com"]
 };
