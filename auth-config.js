329a91b1de72f1fb743f07a3ad49f30f7f26c241
lisaprz0803 <294371049+lisaprz0803@users.noreply.github.com>
2026-06-17 00:35:33 -0400

    Connect agenda to Google Sheets database

--- auth-config.js
+++ auth-config.js
@@ -1,4 +1,6 @@
 window.AGENDA_AUTH_CONFIG = {
   googleClientId: "157406915690-1b0mlv03tppenmtmbout9e9rmedlgtbb.apps.googleusercontent.com",
-  allowedEmails: ["lisaprz0803@gmail.com"]
+  allowedEmails: ["lisaprz0803@gmail.com"],
+  spreadsheetId: "1GRYY_e_2jf9525UWyZO_gPm9_BE8dZuWf7XWwn7iMEk",
+  sheetsScope: "https://www.googleapis.com/auth/spreadsheets"
 };
